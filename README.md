558644ade27f5e5c0367192c0872776d2a165c70
gary <gary.white@lodgit.com>
2021-08-03 11:35:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Kata-Multiplication-table-for-number
---
+++ kataMultiTabs.js
@@ -0,0 +1,7 @@
+function multiTable(number) {
+    for (var i = 1; i <= 10; i++ ) {
+        console.log(i + " * " + number + " = " + i * number + '\n');
+    }
+  }
+
+  console.log(multiTable(5))
